4e17922e8719f3fced2e36c0ea3524b9a911a636
lremi <remileroi.pro@gmail.com>
2025-05-16 12:01:33 +0200

    Add Restaurant Detail page and related features
    - Implemented RestaurantDetail component to display detailed information about a restaurant, including reviews, menu, and owner details.
    - Integrated routing for restaurant details in App component.
    - Updated Restaurants component to link to individual restaurant detail pages.
    - Added loading and error handling states in RestaurantDetail.
    - Created styles for RestaurantDetail page.
    - Added a new image file for restaurant details.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ import Map from './pages/Map';
 import { CurrencyProvider } from './contexts/CurrencyContext';
 import Profile from './pages/Profile';
 import RegisterRestaurant from './pages/RegisterRestaurant';
+import RestaurantDetail from './pages/RestaurantDetail';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
             <Route path="/profile" element={<><NavBar /><Profile /></>} />
             <Route path="/map" element={<><NavBar /><Map/></>} />
             <Route path="/register-restaurant" element={<><NavBar /><RegisterRestaurant/></>} />
+            <Route path="/restaurants/:id" element={<RestaurantDetail />} />
           </Routes>
         </div>
       </Router>
