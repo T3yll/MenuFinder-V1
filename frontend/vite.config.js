bcb9395d3a5253a1e1d08c8073a0a638aa907849
Nathan Bertaud <nathan.bertaud.25@gmail.com>
2025-05-16 11:15:43 +0200

    vite conflit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,8 +7,6 @@ import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-  console.log(mode)
-
   return {
     plugins: [react()],
     server: {
@@ -34,31 +32,4 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-  plugins: [react()],
-    define: {
-      'process.env': JSON.stringify(env)
-    },
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    watch: {
-      usePolling: true
-    },
-    proxy: {
-      '/api': {
-        target: 'http://backend:4000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  }
-}});
+
