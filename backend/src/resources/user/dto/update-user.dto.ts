9287f8636cb4ec344501f5d3a7f475de2ac420f7
lremi <remileroi.pro@gmail.com>
2025-06-26 12:01:27 +0200

    Ajout des validations pour les champs email et bAdmin dans le DTO de mise à jour de l'utilisateur. Amélioration de la structure des validations existantes.

--- backend/src/resources/user/dto/update-user.dto.ts
+++ backend/src/resources/user/dto/update-user.dto.ts
@@ -1,26 +1,34 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsNotEmpty, IsString, IsNumber, IsOptional, MinLength, IsObject } from 'class-validator';
+import { IsNotEmpty, IsString, IsNumber, IsOptional, MinLength, IsObject, IsEmail, IsBoolean } from 'class-validator';
 
 export class UpdateUserDto {
     @IsString()
-      @IsNotEmpty()
-      username: string;
-    
-      @IsString()
-      @IsOptional()
-      @MinLength(8)
-      password: string;
-    
-      @IsString()
-      @IsNotEmpty()
-      nom: string;
-    
-      @IsString()
-      @IsNotEmpty()
-      prenom: string;
-  
+    @IsNotEmpty()
+    username: string;
+
     @IsString()
     @IsOptional()
-   image_file_id: string;
+    @MinLength(8)
+    password: string;
+
+    @IsString()
+    @IsNotEmpty()
+    nom: string;
+
+    @IsString()
+    @IsNotEmpty()
+    prenom: string;
+
+    @IsEmail()
+    @IsOptional()
+    email: string;
+
+    @IsBoolean()
+    @IsOptional()
+    bAdmin: boolean;
+
+    @IsString()
+    @IsOptional()
+    image_file_id: string;
 }
